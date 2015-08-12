2d4aa6a0e1ed11d472bbffc0308b9129a4cf29c5
Marcio Chagas <marciosxagas@gmail.com>
2015-08-12 00:42:52 -0300

    .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protect_from_forgery with: :exception
+
+  helper_method :current_user
+  before_action :ensure_logged_in
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def ensure_logged_in
+    if session.blank? || session[:user_id].blank?
+      redirect_to "/signin"
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150806032605) do
+ActiveRecord::Schema.define(version: 20150812023028) do
 
   create_table "tickets", force: :cascade do |t|
     t.string   "status"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20150806032605) do
     t.string   "category"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "login"
+    t.string   "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
